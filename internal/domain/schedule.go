784c1ac170be626defb59c1705b571e0c306219d
xis <fopeak@gmail.com>
2025-08-29 00:18:33 +0300

    feat(schedule): enhance Schedule struct and add TaskSchedulerService interface with context support

--- internal/domain/schedule.go
+++ internal/domain/schedule.go
@@ -1,6 +1,9 @@
 package domain
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Schedule struct {
 	ID                           string
@@ -15,4 +18,14 @@ type Schedule struct {
 	IsActive                     bool
 	LastModifiedData             string
 	PollingScheduleGap_AsSeconds int
+}
+
+type TaskSchedulerService interface {
+	Schedule(ctx context.Context, params CreateScheduleParams) (Schedule, error)
+	DeleteSchedule(ctx context.Context, scheduleID string, workflowType WorkflowType) error
+	GetSchedule(ctx context.Context, scheduleID string, workflowID string) (Schedule, error)
+	DeleteWorkflowSchedules(ctx context.Context, workflowID string, workflowType WorkflowType) error
+	StartPolling(ctx context.Context) error
+	PollSchedules(ctx context.Context, limit int) error
+	ProcessSchedule(ctx context.Context, schedule Schedule) error
 }
