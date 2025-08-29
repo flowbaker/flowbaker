ce888145676223582bd20ae320ef8ffd793939f1
xis <fopeak@gmail.com>
2025-08-30 00:49:47 +0300

    feat(integrations): add integration registrations

--- cmd/integrations.go
+++ cmd/integrations.go
@@ -3,6 +3,33 @@ package main
 import (
 	"context"
 	"flowbaker/internal/domain"
+	"flowbaker/pkg/integrations/ai_agent"
+	claudeintegration "flowbaker/pkg/integrations/claude"
+	"flowbaker/pkg/integrations/condition"
+	cronintegration "flowbaker/pkg/integrations/cron"
+	"flowbaker/pkg/integrations/discord"
+	"flowbaker/pkg/integrations/dropbox"
+	"flowbaker/pkg/integrations/flowbaker_agent_memory"
+	githubintegration "flowbaker/pkg/integrations/github"
+	"flowbaker/pkg/integrations/google/gmail"
+	googledrive "flowbaker/pkg/integrations/google/google_drive"
+	googlesheets "flowbaker/pkg/integrations/google/google_sheets"
+	"flowbaker/pkg/integrations/google/youtube"
+	"flowbaker/pkg/integrations/http"
+	"flowbaker/pkg/integrations/jira"
+	jwtintegration "flowbaker/pkg/integrations/jwt"
+	"flowbaker/pkg/integrations/knowledge"
+	"flowbaker/pkg/integrations/linear"
+	mongodb "flowbaker/pkg/integrations/mongo"
+	"flowbaker/pkg/integrations/openai"
+	"flowbaker/pkg/integrations/postgresql"
+	"flowbaker/pkg/integrations/redis"
+	resendintegration "flowbaker/pkg/integrations/resend"
+	s3integration "flowbaker/pkg/integrations/s3"
+	sendresponse "flowbaker/pkg/integrations/send_response"
+	slackintegration "flowbaker/pkg/integrations/slack"
+	"flowbaker/pkg/integrations/storage"
+	"flowbaker/pkg/integrations/stripe"
 
 	"github.com/rs/zerolog/log"
 )
@@ -20,7 +47,127 @@ type IntegrationRegisterParams struct {
 	NewConnectionTester          func(deps domain.IntegrationDeps) domain.IntegrationConnectionTester
 }
 
-var integrationRegisterParams = []IntegrationRegisterParams{}
+var integrationRegisterParams = []IntegrationRegisterParams{
+	{
+		IntegrationType:        domain.IntegrationType_Discord,
+		NewCreator:             discord.NewDiscordIntegrationCreator,
+		NewPollingEventHandler: discord.NewDiscordPollingHandler,
+		NewConnectionTester:    discord.NewDiscordConnectionTester,
+	},
+	{
+		IntegrationType: domain.IntegrationType_HTTP,
+		NewCreator:      http.NewHTTPIntegrationCreator,
+	},
+	{
+		IntegrationType: domain.IntegrationType_PostgreSQL,
+		NewCreator:      postgresql.NewPostgreSQLIntegrationCreator,
+	},
+	{
+		IntegrationType:     domain.IntegrationType_Stripe,
+		NewCreator:          stripe.NewStripeIntegrationCreator,
+		NewConnectionTester: stripe.NewStripeConnectionTester,
+	},
+	{
+		IntegrationType: domain.IntegrationType_MongoDB,
+		NewCreator:      mongodb.NewMongoDBIntegrationCreator,
+	},
+	{
+		IntegrationType: domain.IntegrationType_OpenAI,
+		NewCreator:      openai.NewOpenAIIntegrationCreator,
+	},
+	{
+		IntegrationType: domain.IntegrationType_Anthropic,
+		NewCreator:      claudeintegration.NewClaudeIntegrationCreator,
+	},
+	{
+		IntegrationType: domain.IntegrationType_AIAgent,
+		NewCreator:      ai_agent.NewAIAgentCreator,
+	},
+	{
+		IntegrationType: domain.IntegrationType_FlowbakerStorage,
+		NewCreator:      storage.NewStorageIntegrationCreator,
+	},
+	{
+		IntegrationType: domain.IntegrationType_FlowbakerAgentMemory,
+		NewCreator:      flowbaker_agent_memory.NewFlowbakerAgentMemoryIntegrationCreator,
+	},
+	{
+		IntegrationType:            domain.IntegrationType_Dropbox,
+		NewCreator:                 dropbox.NewDropboxIntegrationCreator,
+		NewHTTPOAuthClientProvider: dropbox.NewDropboxHTTPClientProvider,
+	},
+	{
+		IntegrationType:            domain.IntegrationType_Gmail,
+		NewCreator:                 gmail.NewGmailIntegrationCreator,
+		NewHTTPOAuthClientProvider: gmail.NewGmailHTTPClientProvider,
+		NewPollingEventHandler:     gmail.NewGmailPollingHandler,
+	},
+	{
+		IntegrationType: domain.IntegrationType_GoogleSheets,
+		NewCreator:      googlesheets.NewGoogleSheetsIntegrationCreator,
+	},
+	{
+		IntegrationType:     domain.IntegrationType_Drive,
+		NewCreator:          googledrive.NewGoogleDriveIntegrationCreator,
+		NewConnectionTester: googledrive.NewGoogleDriveConnectionTester,
+	},
+	{
+		IntegrationType: domain.IntegrationType_Youtube,
+		NewCreator:      youtube.NewYoutubeIntegrationCreator,
+	},
+	{
+		IntegrationType: domain.IntegrationType_JWT,
+		NewCreator:      jwtintegration.NewJWTIntegrationCreator,
+	},
+	{
+		IntegrationType: domain.IntegrationType_Knowledge,
+		NewCreator:      knowledge.NewKnowledgeIntegrationCreator,
+	},
+	{
+		IntegrationType:     domain.IntegrationType_Linear,
+		NewCreator:          linear.NewLinearIntegrationCreator,
+		NewConnectionTester: linear.NewLinearConnectionTester,
+	},
+	{
+		IntegrationType:     domain.IntegrationType_Jira,
+		NewCreator:          jira.NewJiraIntegrationCreator,
+		NewConnectionTester: jira.NewJiraConnectionTester,
+	},
+	{
+		IntegrationType:     domain.IntegrationType_Redis,
+		NewCreator:          redis.NewRedisIntegrationCreator,
+		NewConnectionTester: redis.NewRedisConnectionTester,
+	},
+	{
+		IntegrationType: domain.IntegrationType_Slack,
+		NewCreator:      slackintegration.NewSlackIntegrationCreator,
+	},
+	{
+		IntegrationType: domain.IntegrationType_Resend,
+		NewCreator:      resendintegration.NewResendIntegrationCreator,
+	},
+	{
+		IntegrationType: domain.IntegrationType_AwsS3,
+		NewCreator:      s3integration.NewS3IntegrationCreator,
+	},
+	{
+		IntegrationType:     domain.IntegrationType_Github,
+		NewCreator:          githubintegration.NewGithubIntegrationCreator,
+		NewConnectionTester: githubintegration.NewGitHubConnectionTester,
+	},
+	{
+		IntegrationType: domain.IntegrationType_SendResponse,
+		NewCreator:      sendresponse.NewSendResponseIntegrationCreator,
+	},
+	{
+		IntegrationType:        domain.IntegrationType_Cron,
+		NewPollingEventHandler: cronintegration.NewCronPollingHandler,
+	},
+	{
+		IntegrationType: domain.IntegrationType_Condition,
+		NewCreator:      condition.NewConditionIntegrationCreator,
+	},
+}
 
 type RegisterIntegrationParams struct {
 	IntegrationSelector domain.IntegrationSelector
